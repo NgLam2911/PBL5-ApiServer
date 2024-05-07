a9dbc357ffbd1787a8da98bcf0d232e8baa3e29b
Ngọc Lam <nglamvn2911@gmail.com>
2024-05-07 14:46:18 +0700

    yo they have float number !

--- data_proccessor.py
+++ data_proccessor.py
@@ -109,7 +109,7 @@ class DataProcessor(Singleton):
         for _, row in raw_df.iterrows():
             xmin, ymin, xmax, ymax, confidence, classs, name = row[['xmin', 'ymin', 'xmax', 'ymax', 'confidence', 'class', 'name']]
             # Caculate the average temperature in the bounding box in 8x8 array (image resolution is 800x600)
-            temp = amg[ymin // 75:ymax // 75, xmin // 100:xmax // 100].mean()
+            temp = amg[int(ymin / 75):int(ymax / 75), int(xmin / 100):int(xmax / 100)].mean()
             df = df.append({'xmin': xmin, 'ymin': ymin, 'xmax': xmax, 'ymax': ymax, 'confidence': confidence, 'class': classs, 'name': name, 'temp': temp}, ignore_index=True)
         return df
     
